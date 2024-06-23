f966dd47fb2e631a2facd28668c63854ec274281
Matías da Silva <matiasdasilva@hotmail.es>
2024-06-23 01:27:17 -0300

    Activity tracking implementation

--- controllers/activityTrackingController.js
+++ controllers/activityTrackingController.js
@@ -1,42 +1,105 @@
+import cassandra from "cassandra-driver";
+import { randomUUID } from "crypto";
+import dotenv from "dotenv";
+dotenv.config();
 
-import cassandra from 'cassandra-driver';
-
+// Create client instance
 const client = new cassandra.Client({
-    contactPoints: ['localhost'],
-    localDataCenter: 'datacenter1',
-    credentials: { username: 'admin', password: 'admin' }
+  contactPoints: [process.env.CASSANDRA_URI],
+  localDataCenter: process.env.CASSANDRA_DATACENTER,
+  credentials: {
+    username: process.env.CASSANDRA_USER,
+    password: process.env.CASSANDRA_PASSWORD,
+  },
 });
 
-const query = "CREATE KEYSPACE IF NOT EXISTS your_keyspace WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3' }";
+const query1 =
+  "CREATE KEYSPACE IF NOT EXISTS tracking WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3' };";
+const query2 =
+  "CREATE TABLE IF NOT EXISTS tracking.user_activity (id UUID PRIMARY KEY, user_id text, game_id text, session_id text, event_type text, event_timestamp text, event_data text, level_achieved text)";
 
-client.execute(query)
-    .then(() => console.log('Keyspace created'))
-    .catch(error => console.error(error))
-    .finally(() => client.shutdown());
+client
+  .execute(query1)
+  .then(() =>
+    client
+      .execute(query2)
+      .then(() => console.log("Table created"))
+      .catch((error) => console.error(error))
+      .finally(() => client.shutdown())
+  )
+  .catch((error) => console.error(error))
+  .finally(() => client.shutdown());
 
 export async function insertActivity(req, res) {
-    // Create a new client instance
+  try {
     const client = new cassandra.Client({
-        contactPoints: ['localhost'],
-        localDataCenter: 'datacenter1',
-        credentials: { username: 'admin', password: 'admin' }
+      contactPoints: [process.env.CASSANDRA_URI],
+      localDataCenter: process.env.CASSANDRA_DATACENTER,
+      keyspace: "tracking",
+      credentials: {
+        username: process.env.CASSANDRA_USER,
+        password: process.env.CASSANDRA_PASSWORD,
+      },
     });
 
     // Toma los datos de la solicitud
-    const { userId, gameId, sessionId, eventType, eventTimestamp, eventData, levelAchieved } = req.body;
+    const {
+      userId,
+      gameId,
+      sessionId,
+      eventType,
+      eventTimestamp,
+      eventData,
+      levelAchieved,
+    } = req.body;
 
     // Define la consulta de inserción
-    const query = 'INSERT INTO user_activity (user_id, game_id, session_id, event_type, event_timestamp, event_data, level_achieved) VALUES (?, ?, ?, ?, ?, ?, ?)';
-    const params = [userId, gameId, sessionId, eventType, eventTimestamp, eventData, levelAchieved];
+    const query =
+      "INSERT INTO user_activity (id, user_id, game_id, session_id, event_type, event_timestamp, event_data, level_achieved) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    const params = [
+      randomUUID(),
+      userId,
+      gameId,
+      sessionId,
+      eventType,
+      eventTimestamp,
+      eventData,
+      levelAchieved,
+    ];
 
     // Ejecuta la consulta
-    try {
-        await client.execute(query, params, { prepare: true });
-        res.status(200).send('Row inserted');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error inserting row');
-    } finally {
-        client.shutdown();
-    }
+    const newActivity = await client.execute(query, params, { prepare: true });
+    res.status(200).json(newActivity);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error inserting row");
+  } finally {
+    client.shutdown();
+  }
+}
+
+export async function getActivity(req, res) {
+  try {
+    const client = new cassandra.Client({
+      contactPoints: [process.env.CASSANDRA_URI],
+      localDataCenter: process.env.CASSANDRA_DATACENTER,
+      keyspace: "tracking",
+      credentials: {
+        username: process.env.CASSANDRA_USER,
+        password: process.env.CASSANDRA_PASSWORD,
+      },
+    });
+
+    // Define la consulta de inserción
+    const query = "SELECT * FROM user_activity";
+
+    // Ejecuta la consulta
+    const activities = await client.execute(query, [], { prepare: true });
+    res.status(200).json(activities.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error inserting row");
+  } finally {
+    client.shutdown();
+  }
 }
--- routes/activityTrackingRoutes.js
+++ routes/activityTrackingRoutes.js
@@ -1,94 +1,11 @@
 import { Router } from "express";
-import { insertActivity } from "../controllers/activityTrackingController.js";
+import {
+  insertActivity,
+  getActivity,
+} from "../controllers/activityTrackingController.js";
 const router = Router();
 
 router.post("/activity", insertActivity);
+router.get("/activity", getActivity);
 
 export default router;
-
-/*  // Dynamic import of cassandra-driver
-import('cassandra-driver').then((cassandra) => {
-    // Crea una instancia del cliente
-    const client = new cassandra.Client({
-        contactPoints: ['localhost'],
-        localDataCenter: 'datacenter1',
-        keyspace: 'activitytrackingdata',
-        credentials: { username: 'admin', password: 'admin' }
-    });
-
-    // Define los datos que quieres insertar
-    const userId = cassandra.types.Uuid.random();
-    const gameId = cassandra.types.Uuid.random();
-    const sessionId = cassandra.types.Uuid.random();
-    const eventType = 'start';
-    const eventTimestamp = new Date();
-    const eventData = { 'score': '1000', 'lives': '3' };
-    const levelAchieved = '1';
-
-    // Aquí es donde un desarrollador podría agregar sus propios indicadores
-    const developerData = { 'new_indicator': 'value' };
-
-    // Combina eventData y developerData
-    const combinedData = { ...eventData, ...developerData };
-    
-    // Define la consulta de inserción
-    const query = 'INSERT INTO user_activity (user_id, game_id, session_id, event_type, event_timestamp, event_data, level_achieved) VALUES (?, ?, ?, ?, ?, ?, ?)';
-    const params = [userId, gameId, sessionId, eventType, eventTimestamp, eventData, levelAchieved];
-
-    // Ejecuta la consulta
-    client.execute(query, params, { prepare: true })
-        .then(result => console.log('Row inserted'))
-        .catch(error => console.error(error))
-        .finally(() => client.shutdown());
-});
-
-/*
-* // Dynamic import of cassandra-driver
-import('cassandra-driver').then((cassandra) => {
-  // Crea una instancia del cliente
-  const client = new cassandra.Client({
-    contactPoints: ['localhost'],
-    localDataCenter: 'datacenter1',
-    keyspace: 'activitytrackingdata',
-    credentials: { username: 'admin', password: 'admin' }
-  });
-
-  // Define los datos que quieres insertar
-  const userId = cassandra.types.Uuid.random();
-  const gameId = cassandra.types.Uuid.random();
-  const sessionId = cassandra.types.Uuid.random();
-  const eventType = 'start';
-  const eventTimestamp = new Date();
-  const eventData = { 'score': '1000', 'lives': '3' };
-  const levelAchieved = '1';
-
-  // Define la consulta de inserción
-  const query = 'INSERT INTO user_activity (user_id, game_id, session_id, event_type, event_timestamp, event_data, level_achieved) VALUES (?, ?, ?, ?, ?, ?, ?)';
-  const params = [userId, gameId, sessionId, eventType, eventTimestamp, eventData, levelAchieved];
-
-  // Ejecuta la consulta
-  client.execute(query, params, { prepare: true })
-    .then(result => console.log('Row inserted'))
-    .catch(error => console.error(error))
-    .finally(() => client.shutdown());
-});
-* 
-* 
-* */
-
-/*
-CREATE TABLE IF NOT EXISTS user_activity (
-    user_id uuid,
-    game_id uuid,
-    session_id uuid,
-    event_type text,
-    event_timestamp timestamp,
-    event_data map<text, text>,
-    PRIMARY KEY ((user_id, game_id), event_timestamp, session_id)
-) WITH CLUSTERING ORDER BY (event_timestamp DESC);
-
-INSERT INTO user_activity (user_id, game_id, session_id, event_type, event_timestamp, event_data)
-VALUES (uuid(), uuid(), uuid(), 'achievement_unlocked', toTimeStamp(now()), {'achievement_name': 'First Steps', 'achievement_points': '10'});
-
-SELECT * FROM user_activity WHERE user_id = ? AND game_id = ?;
-*/
